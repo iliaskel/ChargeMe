16199b88db901fd064ae4867ebe6c47665040272
ikelasov <ilias.kelasidis@uxma.com>
2021-02-03 19:01:41 +0100

    Make view model inject the dispatcher for use

--- app/src/main/java/com/example/chargeme/features/vehicles/viewmodel/VehiclesViewModel.kt
+++ app/src/main/java/com/example/chargeme/features/vehicles/viewmodel/VehiclesViewModel.kt
@@ -1,12 +1,13 @@
 package com.example.chargeme.features.vehicles.viewmodel
 
 import androidx.lifecycle.*
+import com.example.chargeme.core.di.IoDispatcher
 import com.example.chargeme.features.vehicles.mapper.VehicleDomainToUIModelMapper
 import com.example.chargeme.features.vehicles.model.VehicleUI
 import com.example.chargeme.usecases.GetVehiclesListFlowUC
 import com.example.chargeme.usecases.ReplaceVehiclesUC
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -21,13 +22,14 @@ class VehiclesViewModelImpl @Inject constructor(
     private val replaceVehiclesUC: ReplaceVehiclesUC,
     private val getVehiclesListFlowUC: GetVehiclesListFlowUC,
     private val mapper: VehicleDomainToUIModelMapper,
-    private val savedStateHandle: SavedStateHandle
+    private val savedStateHandle: SavedStateHandle,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) : ViewModel(), VehiclesViewModel {
 
     override val vehiclesUILiveData: LiveData<List<VehicleUI>> = observeVehicleUiLiveDataLst()
 
     override fun updateVehicles() {
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(dispatcher) {
             replaceVehiclesUC.execute()
         }
     }
