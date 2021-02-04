14113173044d3848a682ad4048e7a16718e46e8d
ikelasov <ilias.kelasidis@uxma.com>
2021-02-04 17:25:08 +0100

    Grab the secret key form Constants

--- app/src/main/java/com/example/chargeme/data/remotedatasource/api/VehiclesApi.kt
+++ app/src/main/java/com/example/chargeme/data/remotedatasource/api/VehiclesApi.kt
@@ -9,5 +9,5 @@ import javax.crypto.Cipher.SECRET_KEY
 
 interface VehiclesApi {
     @GET("/b/5fa8ff8dbd01877eecdb898f") @Headers("Content-Type: application/json")
-    suspend fun getData(@Header("secret-key") key: String = BuildConfig.SECRET_KEY): Response
+    suspend fun getData(@Header("secret-key") key: String = Constants.secretKey): Response
 }
